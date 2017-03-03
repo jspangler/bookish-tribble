a1649f803552d3550e67579a25a6fda2d6136f97
Jason <jspangler87@gmail.com>
2017-03-03 15:51:54 -0500

    Added more js comments

--- example.js
+++ example.js
@@ -16,12 +16,15 @@ var contents = fs.readFileSync(filename, 'utf8');
 // split on the new line
 lines = contents.split('\n');
 
+// loop through each line to tally up ages
 lines.forEach(function(line) {
   result = line.split(',');
+  // age represents the 2nd index
   var age = result[1];
   ages[age] = ages[age] ? ages[age] + 1 : 1;
 });
 
+// loop through each key printing age and count
 Object.keys(ages).forEach(function(age) {
   console.log(age + ',' + ages[age]);
 });
